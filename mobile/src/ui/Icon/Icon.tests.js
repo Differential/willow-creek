71774c5576e3894c0bf364ac80ae311aeefaa340
Burke Shartsis <burke@burkeshartsis.com>
2018-06-06 00:42:19 -0400

    19 (part 1) UI Icon Library (#37)
    * adding svg support and icon library
    
    * fix broken icon and update 🍪
    
    * fixed loading state story.
    
    * removing Newspring spacific icons
    
    * cleanup
    
    * removing Newspring logo icon
    
    * updated 🍪

---
+++ mobile/src/ui/Icon/Icon.tests.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { kebabCase } from 'lodash';
+
+import Providers from 'TestProviders';
+
+import Icon from './';
+import * as icons from './icons';
+
+Object.keys(icons).forEach((iconName) => {
+  describe(`The ${iconName} icon`, () => {
+    it('renders correctly', () => {
+      const tree = renderer.create(
+        <Providers>
+          <Icon name={kebabCase(iconName)} />
+        </Providers>
+      );
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
---
+++ mobile/src/ui/Icon/index.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { compose, pure } from 'recompose';
+import { flow, camelCase, upperFirst, kebabCase } from 'lodash';
+
+import Placeholder from 'ui/Placeholder';
+import * as Icons from './icons';
+
+const pascalCase = (string) => flow(camelCase, upperFirst)(string);
+
+// Convenience component to render icons based on the icon's string name, like:
+// <Icon name="skip-next" {...allOtherPropsPassedToComponent} />
+//
+// Can also import the icon directly:
+// import { SkipNext } from 'Icon/icons';
+// <SkipNext />
+
+const enhance = compose(pure);
+
+const Icon = enhance(({ name, size, isLoading = false, ...otherProps }) => {
+  const IconComponent = Icons[pascalCase(name)];
+  return (
+    <Placeholder.Media size={size} hasRadius onReady={!isLoading}>
+      <IconComponent size={size} {...otherProps} />
+    </Placeholder.Media>
+  );
+});
+
+Icon.propTypes = {
+  name: PropTypes.oneOf(Object.keys(Icons).map(kebabCase)).isRequired,
+  size: PropTypes.number,
+  fill: PropTypes.string,
+  isLoading: PropTypes.bool,
+};
+
+Icon.defaultProps = {
+  size: 32, // 32 is the default size used within the svg component
+};
+
+export default Icon;
