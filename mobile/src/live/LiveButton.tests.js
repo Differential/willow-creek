f111ec77ad98f60bcca81b8391c770c84e0d3f3f
Daniel Edwards <dan.edwards@newspring.cc>
2018-07-27 15:09:33 -0400

    Issue 110 - Add Test Coverage to Query Components (#159)
    * Issue 110 - Addes 2 tests to LiveButton and Home Query Components
    
    * #110 - Added Query tests to the content-single, content-feed, discover, and home components & also created snapshots.
    
    * Added Snapshots to LiveButton Component
    
    * Updated Discover Component Snapshots
    
    * Updated Snapshots after merging master
    
    * Removed content-single Query test until issue can be resolved

---
+++ mobile/src/live/LiveButton.tests.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Providers from 'Providers';
+
+import LiveNowButton from './LiveButton';
+import getLiveStream from './getLiveStream.graphql';
+
+const mocks = [
+  {
+    request: {
+      query: getLiveStream,
+      variables: {
+        isLiveNow: true,
+      },
+    },
+    result: {
+      data: {
+        liveStream: { isLiveNow: true },
+      },
+    },
+  },
+];
+
+it('renders without error', () => {
+  const tree = renderer.create(
+    <Providers mocks={mocks} addTypename={false}>
+      <LiveNowButton isLiveNow />
+    </Providers>
+  );
+  expect(tree).toMatchSnapshot();
+});
