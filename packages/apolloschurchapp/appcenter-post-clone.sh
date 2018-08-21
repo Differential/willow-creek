eef06f01abf85ca998b9b3de24a03fe0481f9807
Conrad VanLandingham <conrad@differential.com>
2018-08-21 13:46:57 -0500

    force appcenter to recognize yarn (#229)

--- packages/apolloschurchapp/appcenter-post-clone.sh
+++ packages/apolloschurchapp/appcenter-post-clone.sh
@@ -4,3 +4,6 @@ brew uninstall node@6
 NODE_VERSION="8.11.3"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+
+# Appcenter needs a yarn.lock file to exist next to the package.json to detect yarn:
+cp $(pwd)/../../yarn.lock $(pwd)/yarn.lock
