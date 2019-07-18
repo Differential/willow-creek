e26d4f9a8a4c446d39646f9ea369942fd70102cb
Conrad VanLandingham <hi@convan.me>
2019-07-17 22:06:44 -0500

    delete problem story

--- packages/WillowCreekApp/src/ui/FindCampusAd/FindCampusAd.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@apollosproject/ui-storybook';
-import FindCampusAd from '.';
-
-storiesOf('FindCampusAd', module).add('default', () => <FindCampusAd />);
