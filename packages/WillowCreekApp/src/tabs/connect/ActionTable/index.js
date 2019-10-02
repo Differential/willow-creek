474db7bf662e4d10eb0ea915f91c3ed11f8a6d6d
Vincent Wilson <vince@classyh.at>
2019-10-02 07:33:11 -0700

    Email in onboarding (#18)
    * Adds email field to onboarding
    
    * Update data schema package
    
    * Upgrade apollos

--- packages/WillowCreekApp/src/tabs/connect/ActionTable/index.js
+++ packages/WillowCreekApp/src/tabs/connect/ActionTable/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View } from 'react-native';
+import NavigationActions from 'WillowCreekApp/src/NavigationService';
 
 import {
   TableView,
---
+++ packages/WillowCreekApp/src/ui/Onboarding/AskNameConnected.js
@@ -0,0 +1,117 @@
+import React, { memo } from 'react';
+import { Query, Mutation } from 'react-apollo';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import PropTypes from 'prop-types';
+
+import GET_USER_FIRST_AND_LAST_NAME from './getUserFirstAndLastName';
+import AskName from './AskName';
+
+import UPDATE_USER_NAME from './updateUserName';
+
+// eslint-disable-next-line react/display-name
+const AskNameConnected = memo(
+  ({ Component, onPressPrimary, onPressSecondary, ...props }) => (
+    <Query query={GET_USER_FIRST_AND_LAST_NAME}>
+      {({ loading, data: { currentUser = { profile: {} } } = {} }) => {
+        const { firstName, lastName, email } = currentUser.profile;
+
+        return (
+          <Mutation mutation={UPDATE_USER_NAME}>
+            {(updateName) => (
+              <Formik
+                initialValues={{ firstName, lastName, email }}
+                isInitialValid={() => !!(firstName && lastName)} // isInitialValid defaults to `false` this correctly checks for user data
+                validationSchema={Yup.object().shape({
+                  firstName: Yup.string().required(
+                    'Your first name is required!'
+                  ),
+                  lastName: Yup.string().required(
+                    'Your last name is required!'
+                  ),
+                  email: Yup.string().email('Email must be a valid email'),
+                })}
+                enableReinitialize
+                onSubmit={async (
+                  variables,
+                  { setSubmitting, setFieldError }
+                ) => {
+                  try {
+                    await updateName({ variables });
+                    onPressPrimary();
+                  } catch (e) {
+                    const { graphQLErrors } = e;
+                    if (
+                      graphQLErrors.length &&
+                      graphQLErrors.find(({ message }) =>
+                        message.includes('User already exists')
+                      )
+                    ) {
+                      setFieldError(
+                        'firstName',
+                        'There was a problem sending your request'
+                      );
+                    } else {
+                      setFieldError(
+                        'firstName',
+                        'Unknown error. Please try again later.'
+                      );
+                    }
+                  }
+                  setSubmitting(false);
+                }}
+              >
+                {({
+                  isValid,
+                  isSubmitting,
+                  submitForm,
+                  values,
+                  touched,
+                  errors,
+                  setFieldValue,
+                }) => (
+                  <Component
+                    onPressPrimary={loading || isValid ? submitForm : null} // if form `isValid` show the primary nav button (next)
+                    onPressSecondary={
+                      // if form `!isValid` show the secondary nav button (skip)
+                      isValid ? null : onPressSecondary || onPressPrimary // if onPressSecondary exists use it else default onPressPrimary
+                    }
+                    pressPrimaryEventName={'Ask Name Completed'}
+                    pressSecondaryEventName={'Ask Name Skipped'}
+                    firstName={firstName}
+                    lastName={lastName}
+                    values={values}
+                    touched={touched}
+                    errors={errors}
+                    setFieldValue={setFieldValue}
+                    isLoading={loading || isSubmitting}
+                    {...props}
+                  />
+                )}
+              </Formik>
+            )}
+          </Mutation>
+        );
+      }}
+    </Query>
+  )
+);
+
+AskNameConnected.propTypes = {
+  // Custom component to be rendered. Defaults to AskName
+  Component: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+  onPressPrimary: PropTypes.func,
+  onPressSecondary: PropTypes.func,
+};
+
+AskNameConnected.defaultProps = {
+  Component: AskName,
+};
+
+AskNameConnected.displayName = 'AskNameConnected';
+
+export default AskNameConnected;
