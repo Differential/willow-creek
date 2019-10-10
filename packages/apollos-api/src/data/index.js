56e726833f565139e28acf5ae10a1e361443ab74
Vincent Wilson <vince@classyh.at>
2019-10-10 11:32:24 -0400

    Update api

--- packages/apollos-api/src/data/index.js
+++ packages/apollos-api/src/data/index.js
@@ -8,6 +8,7 @@ import * as Cloudinary from '@apollosproject/data-connector-cloudinary';
 import * as OneSignal from '@apollosproject/data-connector-onesignal';
 import * as Search from '@apollosproject/data-connector-algolia-search';
 import * as Pass from '@apollosproject/data-connector-passes';
+import * as Cache from '@apollosproject/data-connector-redis-cache';
 import * as Sms from '@apollosproject/data-connector-twilio';
 import {
   Followings,
@@ -69,6 +70,7 @@ const data = {
   YoutubeImport,
   Group,
   Event,
+  Cache,
 };
 
 // If we are in development mode, we allow users to import data from Youtube.
