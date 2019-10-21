7444a9460965cb7631977edb17613474e5311847
Vincent Wilson <vince@classyh.at>
2019-10-21 11:15:31 -0400

    Home feed refetch (#40)
    * Refetch home feed when changing campuses
    
    * Refetch features as well

--- packages/WillowCreekApp/src/user-settings/Locations/index.js
+++ packages/WillowCreekApp/src/user-settings/Locations/index.js
@@ -6,6 +6,10 @@ import Geolocation from 'react-native-geolocation-service';
 import { PaddedView, ButtonLink } from '@apollosproject/ui-kit';
 import { AnalyticsConsumer } from '@apollosproject/ui-analytics';
 
+import GET_USER_FEED from '../../tabs/home/getUserFeed';
+import GET_FEED_FEATURES from '../../tabs/home/getFeedFeatures';
+import GET_CAMPAIGN_CONTENT_ITEM from '../../tabs/home/getCampaignContentItem';
+
 import GET_CAMPUSES from './getCampusLocations';
 import CHANGE_CAMPUS from './campusChange';
 import MapView from './MapView';
@@ -80,7 +84,20 @@ class Location extends PureComponent {
         fetchPolicy="cache-and-network"
       >
         {({ loading, error, data: { campuses = [] } = {} }) => (
-          <Mutation mutation={CHANGE_CAMPUS}>
+          <Mutation
+            mutation={CHANGE_CAMPUS}
+            refetchQueries={[
+              {
+                query: GET_USER_FEED,
+                variables: {
+                  first: 10,
+                  after: null,
+                },
+              },
+              { query: GET_CAMPAIGN_CONTENT_ITEM, variables: undefined },
+              { query: GET_FEED_FEATURES, variables: undefined },
+            ]}
+          >
             {(handlePress) => (
               <AnalyticsConsumer>
                 {({ track }) => (
