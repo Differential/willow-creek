c64879d6bc4b2e650e6a8aa1df142cd994c85e96
Vincent Wilson <vince@classyh.at>
2019-11-13 09:15:44 -0500

    Setup connection status value to point at 'App user' (#58)
    * Setup connection status value to point at 'App user'
    
    * Finish comment

---
+++ packages/apollos-api/src/data/auth/index.js
@@ -0,0 +1,23 @@
+import { Auth } from '@apollosproject/data-connector-rock';
+
+const { schema, resolver, contextMiddleware } = Auth;
+
+class dataSource extends Auth.dataSource {
+  createUserProfile = async (props = {}) => {
+    console.warn('creating user profile');
+    try {
+      const { email } = props;
+
+      return await this.post('/People', {
+        Email: email,
+        IsSystem: false, // Required by Rock
+        Gender: 0, // Required by Rock
+        ConnectionStatusValueId: 5679, // Points to 'App User' 
+      });
+    } catch (err) {
+      throw new Error('Unable to create profile!');
+    }
+  };
+}
+
+export { schema, resolver, dataSource, contextMiddleware };
