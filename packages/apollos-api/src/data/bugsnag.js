8d1f9cb692f76166965c5c04c9c0982e640eaa1f
Vincent Wilson <vince@classyh.at>
2019-11-13 11:59:20 -0500

    Adds bugsnag to API (#59)
    * add api bugsnag
    
    * Release state -> Release stage

---
+++ packages/apollos-api/src/data/bugsnag.js
@@ -0,0 +1,17 @@
+import bugsnag from '@bugsnag/js';
+import Config from '@apollosproject/config';
+
+const bugsnagClient = bugsnag({
+  apiKey: process.env.BUGSNAG_API_KEY,
+  releaseStage: process.env.RELEASE_STAGE || 'development',
+});
+
+export const report = (error, metaData, beforeSend) => {
+  bugsnagClient.notify(error, {
+    metaData: {
+      Rock: { rockUrl: Config.ROCK.API_URL },
+      ...metaData,
+    },
+    beforeSend,
+  });
+};
