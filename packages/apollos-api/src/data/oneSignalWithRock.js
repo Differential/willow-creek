e069f9cf43c68c692b414d14a1428b36bb6c947c
Vincent Wilson <vince@classyh.at>
2020-02-18 14:05:20 -0500

    Add additional logging

--- packages/apollos-api/src/data/oneSignalWithRock.js
+++ packages/apollos-api/src/data/oneSignalWithRock.js
@@ -4,7 +4,9 @@ export const resolver = {
   Mutation: {
     updateUserPushSettings: async (root, { input }, { dataSources }) => {
       // register the changes w/ one signal
+      console.log({ input });
       const returnValue = await dataSources.OneSignal.updatePushSettings(input);
+      console.log({ returnValue });
 
       // if the pushProviderUserId is changing, we need ot register the device with rock.
       if (input.pushProviderUserId != null) {
