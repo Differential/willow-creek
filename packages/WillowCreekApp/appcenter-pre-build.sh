4da88b05b7ec6a23895d29fffb4f8bce747682aa
Vincent Wilson <vince@classyh.at>
2020-04-26 21:12:37 -0400

    Android setup and app-center tweaking

--- packages/WillowCreekApp/appcenter-pre-build.sh
+++ packages/WillowCreekApp/appcenter-pre-build.sh
@@ -6,15 +6,15 @@ cp $droid_dir/gradle.properties.appcenter $droid_dir/gradle.properties
 
 cd ios
 pod install
-cd ..
+# cd ..
 
 # You can comment this out if your app doesn't have an existing build number.
-VERSION_CODE=$((VERSION_CODE_SHIFT + APPCENTER_BUILD_ID))
+# VERSION_CODE=$((VERSION_CODE_SHIFT + APPCENTER_BUILD_ID))
 
-echo Using "$VERSION_CODE" as build number
+# echo Using "$VERSION_CODE" as build number
 
-echo $VERSION_CODE
-plutil -replace CFBundleVersion -string "$VERSION_CODE" $(pwd)/ios/WillowCreekApp/Info.plist
-sed -i "" 's/versionCode [^"]*/versionCode '$VERSION_CODE'/' $(pwd)/android/app/build.gradle
+# echo $VERSION_CODE
+# plutil -replace CFBundleVersion -string "$VERSION_CODE" $(pwd)/ios/WillowCreekApp/Info.plist
+# sed -i "" 's/versionCode [^"]*/versionCode '$VERSION_CODE'/' $(pwd)/android/app/build.gradle
 
 yarn generate-stories
