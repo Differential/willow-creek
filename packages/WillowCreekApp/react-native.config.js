47a3bcb336ecd1b3f9c890d4817078f0377bd67b
Vincent Wilson <vince@classyh.at>
2020-10-30 10:52:17 -0400

    Update react-native.config to exclude invalid analytics packages

--- packages/WillowCreekApp/react-native.config.js
+++ packages/WillowCreekApp/react-native.config.js
@@ -5,5 +5,11 @@ module.exports = {
         android: null, // disable Android platform, other platforms will still autolink if provided
       },
     },
+    '@metarouter/analytics-react-native': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink if provided
+        ios: null,
+      },
+    },
   },
 };
